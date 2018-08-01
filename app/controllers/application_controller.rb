0b7b90d2cc39889ec991eba82788f7cb33c9297f
nicole03 <ntidwell03@gmail.com>
2018-08-01 18:48:31 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,11 @@
-class ApplicationController < Sinatra::Base
+``class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
 
+get '/food_health' do 
+erb :food_health
 end
+end 
